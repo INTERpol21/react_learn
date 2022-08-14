b0c2a828a4d9bbd580b832a818e55f71362518fe
Anton Zhuravlev <79113636+INTERpol21@users.noreply.github.com>
2022-08-14 18:03:04 +0600

    modified:   src/App.js 	new file:   src/components/PostItem.jsx 	new file:   src/components/PostList.jsx 	new file:   src/styles/App.css

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import React, { useState } from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 
+import './styles/App.css'
+
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
 function App() {
- 
-  //Значение Текста 
-  const [value, setValue] = useState("Текст в инпуте");
-
-
-
+  const[posts, setPosts]= useState([
+    {id:1, title:'JavaScript', body:'Description'},
+    {id:2, title:'JavaScript 2', body:'Description'},
+    {id:3, title:'JavaScript 3', body:'Description'},
+  ])
+  
 
   return (
     <div className="App">
-      <Counter/>
-      <Counter/>
-      
-      <ClassCounter/>
-      <ClassCounter/>
+     <PostList posts={posts} title="Посты JS"/>
+     
     </div>
   );
 }
