eb7c7cfe4f75e35297067f6018bd4a04a4ad9ea6
Anton Zhuravlev <79113636+INTERpol21@users.noreply.github.com>
2022-08-20 22:19:34 +0600

    modified:   src/API/PostService.js 	modified:   src/components/AppRouter.jsx 	new file:   src/components/PostIdPage.jsx 	modified:   src/components/PostItem.jsx 	modified:   src/styles/App.css

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -3,15 +3,16 @@ import { Route, Routes } from 'react-router-dom';
 import Error from "../pages/Error";
 import About from "../pages/About";
 import Posts from "../pages/Posts";
-
+import PostIdPage from './PostIdPage';
 
 const AppRouter = () => {
   return (
-      <Routes>
-          <Route path="/about" element={<About />} />
-          <Route path="/posts" element={<Posts />} />
-          <Route path="/*" element={<Error />} />
-      </Routes>
+    <Routes>
+      <Route path="/about" element={<About />} />
+      <Route path="/posts" element={<Posts />} />
+      <Route path="/posts/:id" element={<PostIdPage />} />
+      <Route path="/*" element={<Error/>} />
+    </Routes>
   )
 
 }
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import MyButton from "./UI/button/MyButton";
 
 const PostItem = (props) => {
+  const router = useNavigate()
+  
+  
   return (
     <div className="post">
       <div className="post__content">
@@ -13,6 +17,9 @@ const PostItem = (props) => {
         </div>
       </div>
       <div className="post__btns">
+        <MyButton onClick={() => router(`/posts/${props.post.id}`)}>
+          Открыть
+        </MyButton>
         <MyButton onClick={() => props.remove(props.post)}>
           Удалить
         </MyButton>
